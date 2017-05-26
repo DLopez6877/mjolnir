873c5a7676d93140b05c5883eccbda6377efd8dd
Daniel <dlopez6877@gmail.com>
2017-05-26 16:37:10 -0700

    add class to form in index.html / add reset fields and alert on sumbit to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,23 @@
 ////////////////////   USER INTERFACE   ////////////////////
 ////////////////////////////////////////////////////////////
 $(function() {
+  function resetFields() {
+    $("input#name").val("");
+    $("input#address").val("");
+    $("input#phone").val("");
+    $("input#email").val("");
+  }
+
   $(".quantity").on('change', function (e) {
     var optionSelected = $("option:selected", this);
     var valueSelected = this.value;
     $("#quantityInput").text(valueSelected);
     $("#priceInput").text("$" + (valueSelected * 20) + ".00");
+  });
+
+  $("form.form1").submit(function(e) {
+    e.preventDefault();
+    resetFields();
+    alert("Sorry this is not a real product.")
   });
 });
