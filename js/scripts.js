addbb873ab974f337689adc43d33f75e61e837ce
Daniel <dlopez6877@gmail.com>
2017-05-28 03:24:29 -0700

    edit footer, add offer details, button style change, additional functionality to table

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 ////////////////////////////////////////////////////////////
 ////////////////////   USER INTERFACE   ////////////////////
 ////////////////////////////////////////////////////////////
@@ -10,10 +11,25 @@ $(function() {
   }
 
   $(".quantity").on('change', function (e) {
-    var optionSelected = $("option:selected", this);
     var valueSelected = this.value;
+    var price = Math.round((valueSelected * 19.95)*100)/100;
+    var shipping = Math.round((valueSelected * 5.65)*100)/100;
+
     $("#quantityInput").text(valueSelected);
-    $("#priceInput").text("$" + (valueSelected * 20) + ".00");
+    $("#shipping").text("$" + shipping);
+    $("#priceInput").text("$" + price);
+    $("#totalPrice").text("$" + (price + shipping + "0"));
+
+    if (valueSelected === "2" || valueSelected === "4") {
+      $("#shipping").text("$" + shipping + "0");
+      $("#priceInput").text("$" + price + "0");
+    }
+
+    if (valueSelected != 1) {
+      $("#quantityFree").text("1");
+    } else {
+      $("#quantityFree").text("0");
+    }
   });
 
   $("form.form1").submit(function(e) {
@@ -21,4 +37,19 @@ $(function() {
     resetFields();
     alert("Sorry this is not a real product.")
   });
+
+  (function countdown(remaining) {
+    var minutes = 10;
+    if (remaining >= 0) {
+      setTimeout(function(){ countdown(remaining - 1); }, 1000);
+      document.getElementById('seconds').innerHTML = remaining;
+      if (document.getElementById('seconds').innerHTML.length <= 1) {
+        document.getElementById('seconds').innerHTML = "0" + remaining;
+      }
+    } else {
+      minutes = minutes - 1;
+      document.getElementById('minutes').innerHTML = minutes;
+      countdown(remaining = 59);
+    }
+  })(0);
 });
